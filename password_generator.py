48ee60a28f0b7750d3b1f97655876e025e740f03
Ethan Saunders <ethansaunders1209@gmail.com>
2026-03-01 21:46:29 +0000

    Add ability to generate multiple passwords

--- password_generator.py
+++ password_generator.py
@@ -6,12 +6,16 @@ numbers = "0123456789"
 symbols = "!@#$%^&()_+-="
 all_chars = letters + numbers + symbols # Combine everything
 
-# Ask the user how long they want the password
-length = int(input("How long do you want your password to be?"))
-print("Password length set to:", length)
+#Ask the user for the number of passwords they want
+numPasswords = int(input("How many passwords do you want? "))
 
-# Generate the random password
-password = ""
-for i in range(length):
-    password += random.choice(all_chars)
-print("Your password is:", password)
+for _ in range(numPasswords):
+    # Ask the user how long they want the password
+    length = int(input("How long do you want your password to be? "))
+    print("Password length set to:", length)
+
+    # Generate the random password
+    password = ""
+    for _ in range(length):
+        password += random.choice(all_chars)
+    print("Your password is:", password)
